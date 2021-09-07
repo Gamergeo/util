90bbaf1231482e919046225697f3f2ed6e5d4d62
gleva <gleva@192.168.0.25>
2021-09-07 21:13:15 +0200

    Gestion des dates

---
+++ util/src/main/resources/js/global/parseDate.js
@@ -0,0 +1,18 @@
+/**
+ * Transforme une date du format dd/mm/yyyy en mm/dd/yyyy
+ */
+$.parseDate = function(stringDate) {
+	
+	let parts = stringDate.match(/(\d+)/g);
+	
+	if (parts[3]) {
+		
+		if (parts[4]) {
+ 			return new Date(parts[2], parts[1]-1, parts[0], parts[3], parts[4]);
+		}
+ 		return new Date(parts[2], parts[1]-1, parts[0], parts[3]);
+	}
+	
+ 	return new Date(parts[2], parts[1]-1, parts[0]);
+}
+
